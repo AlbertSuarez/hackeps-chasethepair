4dc58191844893960d202d8a912e3bddd856c496
Albert Suarez <alsumo95@gmail.com>
2019-10-30 12:13:50 +0100

    Changed solver cause I did not understand the problem well

--- src/chase_the_pair.py
+++ src/chase_the_pair.py
@@ -1,10 +1,2 @@
 def solve(set_a, set_b, to_chase):
-    # Merge two sets
-    list_merged = set_a + set_b
-    # Get closest number and remove it
-    closest_number = min(list_merged, key=lambda x: abs(x - to_chase))
-    del list_merged[list_merged.index(closest_number)]
-    # Get second closest number
-    second_closest_number = min(list_merged, key=lambda x: abs(x - to_chase))
-    # Return
-    return second_closest_number, closest_number
+    return min(set_a, key=lambda x: abs(x - to_chase)), min(set_b, key=lambda x: abs(x - to_chase))
